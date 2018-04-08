6e26ad610a0352a764cc6781d65e300697dcf667
BananaLF <864685021@qq.com>
2018-04-08 17:26:21 +0800

    modify copyright

--- src/init.cpp
+++ src/init.cpp
@@ -527,8 +527,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
-    const std::string URL_WEBSITE = "<https://bitcoincore.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/BitcoinGod/BitcoinGod>";
+    const std::string URL_WEBSITE = "<https://www.bitcoingod.org/>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
