3c5591631387b5c1e8465c06c29a9fb461ebbb8f
BananaLF <864685021@qq.com>
2018-04-09 08:28:52 +0800

    modify default datadir

--- src/util.cpp
+++ src/util.cpp
@@ -530,7 +530,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "BitcoinGod";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -540,10 +540,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/BitcoinGod";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".bitcoingod";
 #endif
 #endif
 }
